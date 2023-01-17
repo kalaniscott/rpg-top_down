527027d0e92c586ae6e4999ff7076046eef6cccd
Kalani Scott <kalaniscott@gmail.com>
2023-01-16 20:50:26 -0600

    Add .tmx files into media subfolder

---
+++ media/tiled_maps/world_map_assets-revolution.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="world_map_assets" tilewidth="8" tileheight="8" tilecount="48" columns="8">
+ <image source="../../Downloads/media/revolution/revolution_tiles.png" width="64" height="48"/>
+</tileset>
